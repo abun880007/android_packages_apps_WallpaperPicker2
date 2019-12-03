c44b59168b90b7f6c84f9d092ff4fdcd691604af
Anish Pratheepan <42488259+ultranoob-5@users.noreply.github.com>
2019-12-03 15:49:42 +0530

    Merge tag 'android-10.0.0_r16'
    Android 10.0.0 Release 16 (QQ1A.191205.011)

--- src/com/android/wallpaper/picker/CategoryFragment.java
+++ src/com/android/wallpaper/picker/CategoryFragment.java
@@ -233,6 +233,7 @@ public class CategoryFragment extends ToolbarFragment {
         }
         // Not add existing category to category list
         if (mCategories.indexOf(category) >= 0) {
+            updateCategory(category);
             return;
         }
 
